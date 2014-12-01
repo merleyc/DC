f4be9dadb5502b49776010edf35f183b950ad80d
merleyc <merleyc@yahoo.com.br>
2014-11-30 22:05:47 -0800

    Adding test message.

--- src/crawler/MyCrawler.java
+++ src/crawler/MyCrawler.java
@@ -8,7 +8,7 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
-
+//testing mel
 public class MyCrawler extends WebCrawler {
 
 		private int counterFile = 0;
